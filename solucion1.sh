49490ee666880459c8f96c755aaaa55ed8addb0c
Alberto Villegas <alvich@floridauniversitaria.es>
2023-01-27 16:59:33 +0100

    Añadiendo script para saber las carpetas de ocupacion de 10% a 35%

---
+++ solucion1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+maxLineas=`cat disc.txt | wc -l`
+
+echo "Las carpetas de ocupacion de disco entre 10% al 35% son: "
+sed -i 's/%/ %/g' disc.txt
+for i in `seq 1 $maxLineas`
+do
+    ficheros=`cat disc.txt | awk '{print($1)}' | head -n$i | tail -n1`
+    porcentaje=`cat disc.txt | awk '{print($5)}' | head -n$i | tail -n1`
+    if [ $porcentaje == Uso ]
+    then
+    echo ""
+    else
+    if [ $porcentaje -ge 10 ] && [ $porcentaje -le 35 ]
+    then
+    echo "$ficheros"
+    fi
+    fi
+done
+sed -i 's/ %/%/g' disc.txt
